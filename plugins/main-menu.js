c3dd6d1aefd679ac56457b2a26943a2e43cd1a5a
fedelan555 <federicojpg1500@gmail.com>
2025-07-08 17:18:50 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -98,8 +98,8 @@ let handler = async (m, { conn, usedPrefix: _p }) => {
       image: { url: 'https://files.catbox.moe/7qo46s.jpg' },
       caption: text,
       buttons: [
-        { buttonId: `${_p}owner`, buttonText: { displayText: '👑 OWNER' }, type: 1 },
-        { buttonId: `${_p}Grupos`, buttonText: { displayText: '👻 GRUPOS' }, type: 1 }
+        { buttonId: `${_p}owner`, buttonText: { displayText: '👑 CREADOR' }, type: 1 },
+        { buttonId: `${_p}Grupos`, buttonText: { displayText: '🧩 GRUPOS' }, type: 1 }
       ],
       viewOnce: true
     }, { quoted: m })
