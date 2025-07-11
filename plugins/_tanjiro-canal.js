ef59e25cbb90ea0df2065485318c7258f56c4b20
fedelan555 <federicojpg1500@gmail.com>
2025-07-11 15:55:49 -0300

    Create _tanjiro-canal.js

---
+++ plugins/_tanjiro-canal.js
@@ -0,0 +1,47 @@
+import { generateWAMessageFromContent, proto } from '@whiskeysockets/baileys'
+
+const handler = async (m, { conn }) => {
+  const texto = `✨ Pulsa el botón para unirte al canal oficial`.trim()
+
+  const messageContent = {
+    viewOnceMessage: {
+      message: {
+        messageContextInfo: {
+          deviceListMetadata: {},
+          deviceListMetadataVersion: 2
+        },
+        interactiveMessage: proto.Message.InteractiveMessage.create({
+          body: proto.Message.InteractiveMessage.Body.create({ text: texto }),
+          footer: proto.Message.InteractiveMessage.Footer.create({ text: 'Pikachu Bot by Deylin' }),
+          header: proto.Message.InteractiveMessage.Header.create({ hasMediaAttachment: false }),
+          nativeFlowMessage: proto.Message.InteractiveMessage.NativeFlowMessage.create({
+            buttons: [
+              {
+                name: 'cta_url',
+                buttonParamsJson: JSON.stringify({
+                  display_text: '✐ Canal oficial',
+                  url: 'https://whatsapp.com/channel/0029VbApe6jG8l5Nv43dsC2N',
+                  merchant_url: 'https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31'
+                })
+              }
+            ]
+          })
+        })
+      }
+    }
+  }
+
+  const msg = generateWAMessageFromContent(m.chat, messageContent, {
+    userJid: m.sender,
+    quoted: m
+  })
+
+  await conn.relayMessage(m.chat, msg.message, { messageId: msg.key.id })
+}
+
+handler.command = /^([.#/!])?canal$/i
+handler.register = true
+handler.help = ['canal']
+handler.tags = ['info']
+
+export default handler
