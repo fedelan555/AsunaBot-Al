ffc86f04cbd6202316edc00f13ec1ff526f01f16
fedelan555 <federicojpg1500@gmail.com>
2025-07-11 12:46:15 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -30,10 +30,3 @@ handler.command = ['menu', 'menú', 'help', 'ayuda']
 handler.register = true
 
 export default handler
-
-function clockString(ms) {
-  let h = isNaN(ms)? '--': Math.floor(ms / 3600000)
-  let m = isNaN(ms)? '--': Math.floor(ms / 60000) % 60
-  let s = isNaN(ms)? '--': Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
-}
