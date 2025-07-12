bbd91baa353c4bb6f773c1a089a7d6ad7ae7c5fe
fedelan555 <federicojpg1500@gmail.com>
2025-07-12 17:36:26 -0300

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -23,7 +23,7 @@ const defaultMenu = {
 ┃ ᴍᴇɴᴜ ʀᴇsᴘɪʀᴀᴄɪᴏɴ ᴅᴇʟ sᴏʟ
 ╰━━━━━━━━━━━━━━━━━━━━╯
 
-👤 Usuario: *%name*  *%greeting*
+👤 Usuario: *%name*
 ⚔  *ʙᴏᴛ*: ${(conn.user.jid == global.conn.user.jid ? '`ᴏғɪᴄɪᴀʟ 🅞`' : '`sᴜʙ - ʙᴏᴛ 🅢`')}
 🌟 Exp: %exp/%maxexp
 🌐 Modo: %mode
@@ -120,4 +120,4 @@ function clockString(ms) {
   let m = isNaN(ms)? '--': Math.floor(ms / 60000) % 60
   let s = isNaN(ms)? '--': Math.floor(ms / 1000) % 60
   return [h, m, s].map(v => v.toString().padStart(2, '0')).join(':')
-      }
+}
