6f41366e825827fd335bf8ed3924fc7052aaf055
fedelan555 <federicojpg1500@gmail.com>
2025-07-14 17:39:20 -0300

    Update Grupo•link.js

--- plugins/Grupo•link.js
+++ plugins/Grupo•link.js
@@ -1,15 +1,27 @@
-var handler = async (m, { conn, args }) => {
+var handler = async (m, { conn}) => {
+  let group = m.chat;
+  let code = await conn.groupInviteCode(group);
+  let link = 'https://chat.whatsapp.com/' + code;
 
-let group = m.chat
-let link = 'https://chat.whatsapp.com/' + await conn.groupInviteCode(group)
-conn.reply(m.chat, '🚩\v' + link, m, rcanal, { detectLink: true })
-
+  await conn.sendMessage(m.chat, {
+    image: { url: 'https://files.catbox.moe/sbzc3p.jpg'}, // Imagen temática estilo Tanjiro
+    caption: `🚩 *Enlace del grupo:*\n${link}\n\n🌸 *Tanjiro Bot* protege tu aliento digital.`,
+    buttons: [
+      {
+        buttonId: '#menucompleto',
+        buttonText: { displayText: '🌸 MENU COMPLETO'},
+        type: 1
 }
-handler.help = ['link']
-handler.tags = ['grupo']
-handler.command = ['link']
+    ],
+    viewOnce: true
+}, { quoted: m});
+};
 
-handler.group = true
-handler.botAdmin = true
+handler.help = ['link'];
+handler.tags = ['grupo'];
+handler.command = ['link'];
 
-export default handler
+handler.group = true;
+handler.botAdmin = true;
+
+export default handler;
